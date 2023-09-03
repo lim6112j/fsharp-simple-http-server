5a6d35b493a2c3ca1f002172a4d75bc13ad60551
ben lim <lim6112j@gmail.com>
2023-09-03 13:38:40 +0900

    not working

--- Program.fs
+++ Program.fs
@@ -3,35 +3,45 @@ open System
 open System.IO
 open System.Net
 open System.Net.Sockets
-let acceptClient (client : TcpClient) handler = async {
-    use stream = client.GetStream()
-    use reader = new StreamReader(stream)
-    let header = reader.ReadLine()
-    if not (String.IsNullOrEmpty(header)) then
-        use writer = new StreamWriter(stream)
-        handler (header, writer)
-        writer.Flush()
-}
-let startServer(address, port) handler =
+
+let acceptClient (client: TcpClient) handler =
+    async {
+        use stream = client.GetStream()
+        use reader = new StreamReader(stream)
+        let header = reader.ReadLine()
+
+        if not (String.IsNullOrEmpty(header)) then
+            use writer = new StreamWriter(stream)
+            handler (header, writer)
+            writer.Flush()
+    }
+
+let startServer (address, port) handler =
     let ip = IPAddress.Parse(address: string)
     let listener = TcpListener(ip, port)
     listener.Start()
+
     async {
         while true do
             let! client = listener.AcceptTcpClientAsync() |> Async.AwaitTask
             acceptClient client handler |> Async.Start
     }
     |> Async.Start
+
 type StreamWriter with
+
     member writer.BinaryWrite(bytes: byte[]) =
-        let writer = new BinaryWriter(writer.BaseStream)
-        writer.Write(bytes)
+        let writerr = new BinaryWriter(writer.BaseStream)
+        writerr.Write(bytes)
+
 let staticContentHandler root (header: string, response: StreamWriter) =
     let parts = header.Split(' ')
     let resource = parts.[1]
     let path = Path.Combine(root, resource.TrimStart('/').TrimStart('\\'))
+
     if File.Exists(path) then
         response.Write("HTTP/1.1 200 OK\r\n\r\n")
+
         if resource.EndsWith(".png") then
             let bytes = File.ReadAllBytes(path)
             response.BinaryWrite(bytes)
@@ -40,4 +50,5 @@ let staticContentHandler root (header: string, response: StreamWriter) =
             response.Write(text)
     else
         response.Write("HTTP/1.1 404 Not Found \r\n\r\n" + resource + " not found.")
-startServer("127.0.0.1", 8080) (staticContentHandler @"./")
+
+startServer ("127.0.0.1", 8080) (staticContentHandler @"/Users/codeguru/")
